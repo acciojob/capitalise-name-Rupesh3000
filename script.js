532a2bc50bf65fb29ffa24cdd451b250a9c61d33
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2025-07-05 23:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let input = document.getElementById("fname");
+
+input.addEventListener("blur", function (event) {
+  let lowwerValue = event.target.value;
+  let upperValue = lowwerValue.toUpperCase();
+  event.target.value = upperValue
+});
